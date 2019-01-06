f741a8835b4023c69517c5b2a4bb3de38d43fc50
Dirk-Willem van Gulik <dirkx@webweaving.org>
2019-01-06 20:25:01 +0100

    Various tweak to change callback into loop

--- src/OptoDebounce.cpp
+++ src/OptoDebounce.cpp
@@ -1,23 +1,80 @@
 #include <OptoDebounce.h>
 #include "Arduino.h"
+#include <FunctionalInterrupt.h>
+
+// #include <portmacro.h>
 
 class OptoDebounce;
 
 // Glue logic (ESP32's ticker does not yet have TArgs). See
 // https://github.com/esp8266/Arduino/pull/5030 for what 
-// is needed.
+// is needed. Likewise in case we use interupts.
 //
-static void _sample(uint32_t arg) {
-	OptoDebounce * c = (OptoDebounce*)arg;
+#ifdef OB_POLLING
+static void _sample(OptoDebounce * c) {
 	c->sample();
 };
+#else
+
+#ifdef ESP32
+portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
+#endif
+
+#ifdef ESP32
+void IRAM_ATTR _handleInterrupt(void * arg) {
+#else
+void ICACHE_RAM_ATTR _handleInterrupt(void * arg) {
+#endif
+        OptoDebounce * c = (OptoDebounce *)arg;
+#ifdef ESP32
+        portENTER_CRITICAL_ISR(&mux); // prolly a bit over the top.
+#else 
+	noInterrupts();
+#endif
+        c->_interruptCounter++;
+#ifdef ESP32
+        portEXIT_CRITICAL_ISR(&mux);
+#else
+	interrupts();
+#endif
+}
+#endif
 
 OptoDebounce::OptoDebounce(uint8_t pin, uint16_t graceMs, uint16_t sampleFrequency ) 
 	: _pin(pin),  _msTrigger(graceMs), _hz(sampleFrequency)
 {
+     pinMode(_pin, INPUT_PULLUP);
+#ifdef OB_POLLING
+     assert(_hz >= 500);
+
      _ticker = new Ticker();
-     _ticker->attach_ms(1000 / _hz /* mSecond */, _sample,(uint32_t) this);
-     pinMode(_pin, INPUT_PULLUP); // analog input.
+     _ticker->attach_ms(1000 / _hz /* mSecond */, _sample,this);
+#else
+#ifdef ESP32
+     attachInterruptArg(digitalPinToInterrupt(_pin), &_handleInterrupt, this, RISING);
+#else
+     attachInterrupt(digitalPinToInterrupt(_pin), std::bind(&OptoDebounce::_handleInt,this), RISING);
+#endif
+#endif
+     _sum = 0;
+}
+
+#ifdef ESP32
+void OptoDebounce::_handleInt() {
+#else
+void ICACHE_RAM_ATTR OptoDebounce::_handleInt() {
+#endif
+#ifdef ESP32
+        portENTER_CRITICAL_ISR(&mux); // prolly a bit over the top.
+#else 
+	noInterrupts();
+#endif
+        _interruptCounter++;
+#ifdef ESP32
+        portEXIT_CRITICAL_ISR(&mux);
+#else
+	interrupts();
+#endif
 }
 
 OptoDebounce::~OptoDebounce() {
@@ -41,34 +98,68 @@ OptoDebounce& OptoDebounce::onOptoChange(THandlerFunction_CallbackWithState fn) 
      return *this;
 };
 
-// Keep this routine as short and concise as posible; as we run on
-// an interrupt/timer.
-//
-void OptoDebounce::sample() {
-     _sum += digitalRead(_pin) ? 1 : 0;
-     _n ++;
+#ifdef OB_POLLING
+void OptoDebounce::_sample() {
+     int m = 100; // _msTrigger *  _hz / 1000;
 
-     if (_n < _msTrigger *  _hz / 1000)
+     _sum = (_sum * m + digitalRead(_pin)) / (m + 1);
+     bool newState = (_sum > 0.8 ) ? true : false;
+
+     if (newState == _state)
 	return;
 
-     bool nstate = (_sum < _n / 2) ? false : true;
-     _n = 0;
-     _sum = 0;
+     _process(newState);
+}
+#endif
 
-     if (nstate == _state)
+void OptoDebounce::loop() {
+#if 1
+     // should see around 100 passes through null per
+     // second if there is an AC voltage, otherwhise
+     // permanentish high.
+     // 
+     if (millis() - last < 1000) 
 	return;
+     last = millis();
 
-     _state = nstate;
+     unsigned int c = _interruptCounter;
+     _interruptCounter = 0;
+
+     // we're seeing 20 (of the about 50) expected pass
+     // through 0 interrupts. So there is prolly power.
+     //
+     bool newState = (c > 5);
+
+     if (newState == _state)
+	return;
+#else
+     if (millis() - last < 250) 
+        return;
+     last = millis();
+     int c = 0;
+     for(int i = 0; i < 40; i++) {
+		c += digitalRead(_pin);
+		delay(1);
+     };
+     // Pulled low most of the time ?
+     bool newState = c < 20;
+#endif
+
+     _state = newState;
      if (_callback)
-       _callback(nstate ? ON : OFF);
+       _callback(newState ? ON : OFF);
 
-     if (nstate && _callbackOn)
+     if (newState && _callbackOn)
        _callbackOn();
 
-     if (!nstate && _callbackOff)
+     if (!newState && _callbackOff)
        _callbackOff();
 };
 
 bool OptoDebounce::state() {
      return _state;
+};
+
+double OptoDebounce::s() {
+     return _sum;
 };
--- src/OptoDebounce.h
+++ src/OptoDebounce.h
@@ -1,7 +1,9 @@
 #ifndef _H_OPTO_DEBOUNCE
 #define _H_OPTO_DEBOUNCE
 
+#ifndef FAXE_TEST
 #include <Ticker.h>
+#endif
   
 #include <algorithm>
 #include <list>
@@ -18,19 +20,27 @@ class OptoDebounce {
    OptoDebounce& onOptoOff(THandlerFunction_Callback fn);
 
    typedef enum { OFF, ON, UNKNOWN } state_t;
-
    typedef std::function<void(state_t state)> THandlerFunction_CallbackWithState;
    OptoDebounce& onOptoChange(THandlerFunction_CallbackWithState fn);
 
    bool state();
    void sample();
+   void loop();
+   double s();
+
+   uint16_t _interruptCounter;
+   void _handleInt();
+   void _process(bool newState);
+
  private:
    Ticker * _ticker;
+
    uint8_t _pin;
    uint16_t _msTrigger;
    uint16_t _hz;
-   uint32_t _n, _sum;
+   float _sum;
    bool _state;
+   unsigned long last;
 
    THandlerFunction_Callback _callbackOn,  _callbackOff;
    THandlerFunction_CallbackWithState _callback;
